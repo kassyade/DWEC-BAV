8b3bd6439cdce1212af20e0c3db8fb06d3f69c34
kassyade <benjamin.aviles.std@gmail.com>
2024-09-26 00:22:17 +0200

    x

--- Tema2/EJ2.js
+++ Tema2/EJ2.js
@@ -1,11 +1,11 @@
 let a = parseInt(prompt("Introduce el primer número"));
-let b = parseInt(prompt("Introduce el primer número"));
-let c = parseInt(prompt("Introduce el primer número"));
+let b = parseInt(prompt("Introduce el segundo número"));
+let c = parseInt(prompt("Introduce el tercer número"));
 let result ="";
 if(a>10|| b>10 || c>10){
 result="Algunos de los número introducidos es mayor que 10";
 }else{
     result="Ninguno de los números es mayor que 10";
 }
-document.getElementById("resultado").innerHTML = result;
+document.getElementById("result").innerHTML = result;
 document.getElementById("num").innerHTML = `Números introducidos: ${a}, ${b}, ${c}`;
