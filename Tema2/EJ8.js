85e9fbc2f82289664c0c513b3614faf77c187a8b
Kas <166696678+kassyade@users.noreply.github.com>
2024-09-26 15:47:33 +0200

    x

--- Tema2/EJ8.js
+++ Tema2/EJ8.js
@@ -1,6 +1,26 @@
-let ano= prompt("Introduce el año")
+let ano= parseInt(prompt("Introduce el año"));
 
+function esBisiesto(ano){
+    if (ano%4===0){
+        if(ano%100===0){
+            if(ano%400==0){
+                return 1;
+            }else{
+                return 0;
+            }
 
-function esBisiesto(){
+        }else{
+            return 1;
+        }
 
+    }else {
+        return 0;
+    }
 }
+ 
+if (esBisiesto(ano)===1){
+    console.log("Este año si es bisiesto");
+}else{
+    console.log("No es bisiesto");
+}
+
