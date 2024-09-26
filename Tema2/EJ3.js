6bde362d6c259c781985927d29731230b25fa380
kassyade <benjamin.aviles.std@gmail.com>
2024-09-26 08:59:50 +0200

    x

--- Tema2/EJ3.js
+++ Tema2/EJ3.js
@@ -1,1 +1,15 @@
-let
+let dia,mes;
+let resultado="";
+while(true){
+     dia =parseInt(prompt("Introduce el día"));
+     mes =parseInt(prompt("Introduce el mes"));
+
+    if(dia===14&& mes===2){
+        resultado="Exacto esa es la fecha de san valentin"
+        break;
+    }else{
+        alert("Fecha incorrecta ,intenta otra vez")
+    }
+    
+}
+document.getElementById("resultado").innerHTML = resultado;
