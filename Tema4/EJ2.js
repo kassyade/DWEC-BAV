f684a4c95004a50643b73c71ac47354c154956d8
kassyade <benjamin.aviles.std@gmail.com>
2024-10-18 12:32:29 +0200

    x

--- Tema4/EJ2.js
+++ Tema4/EJ2.js
@@ -1,17 +1,9 @@
 function crear(tipo, contenido) {
     let hijo = document.createElement(tipo);
     hijo.innerHTML = contenido;
-    return hijo; // Devuelve el elemento creado
 }
-
-// Crea los elementos li
-let primerElemento = crear("li", "primer elemento");
-let segundoElemento = crear("li", "segundo elemento");
-
-// Crea la lista ul y agrega los elementos li
-let lista = document.createElement("ul"); // Corrige createElement a document.createElement
-lista.appendChild(primerElemento); // Usa las variables para agregar los elementos
-lista.appendChild(segundoElemento);
-
-// Agrega la lista al cuerpo del documento
-document.body.appendChild(lista);
+crear("li","primer elemento ")
+crear("li","segundo elemento ")
+let lista = createElement("ul")
+lista.appendChild(li)
+document.body.appendChild(lista)
