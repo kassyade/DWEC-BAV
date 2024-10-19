9184e2d3133c8c11619bb786016ea7b6733cc6d6
kassyade <benjamin.aviles.std@gmail.com>
2024-10-19 11:12:30 +0200

    x

--- Tema4/Ejercicios.js
+++ Tema4/Ejercicios.js
@@ -11,8 +11,8 @@ document.body.appendChild(h1);//lo posicionamos
 let hr1 = crearElemento("hr","")
 document.body.appendChild(hr1)
 
-let h2 = crearElemento("h2","Primer ejercicio ")
-document.body.appendChild(h2)
+let h2a = crearElemento("h2","Primer ejercicio ")
+document.body.appendChild(h2a)
  
 let p1 = crearElemento("p","Utilizando funciones del dom crear la siguiente página, utilizando como referencia la información almacenada en el siguiente set:")
 document.body.appendChild(p1)
@@ -37,3 +37,28 @@ let p1b3 = crearElemento("p","primer parrafo")
 document.body.appendChild(p1b3)
 let p1c4 = crearElemento("p","primer parrafo")
 document.body.appendChild(p1c4)
+/**segundo ejercicio */
+
+let h2= crearElemento("h2","Segundo ejercicio")
+document.body.appendChild(h2)
+let p2 = crearElemento("p","Haz un programa que cuando se pulse un botón “InsertarAleatorio”, añada un elemento con un número aleatorio a una lista desordenada (elemento UL).")
+document.body.appendChild(p2)
+
+/**lista ul */
+let ul = crearElemento("ul","")
+document.body.appendChild(ul)
+let li = crearElemento("li","elemento 1")
+ul.appendChild(li)
+
+function nuevo(){
+let num = Math.floor(Math.random()* 101)//numero random
+let a =crearElemento("li","nuevo elemento"+num)
+ul.appendChild(a)
+}
+
+let boton= crearElemento("button","click!!")
+document.body.appendChild(boton)
+
+boton.addEventListener("click", nuevo);
+
+
