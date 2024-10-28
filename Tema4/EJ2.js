b5e42ed402ff30e0a440dc6cba9090c0792b6b38
Kas <166696678+kassyade@users.noreply.github.com>
2024-10-28 09:06:50 +0100

    x

--- Tema4/EJ2.js
+++
@@ -1,9 +0,0 @@
-function crear(tipo, contenido) {
-    let hijo = document.createElement(tipo);
-    hijo.innerHTML = contenido;
-}
-crear("li","primer elemento ")
-crear("li","segundo elemento ")
-let lista = createElement("ul")
-lista.appendChild(li)
-document.body.appendChild(lista)
--- Tema4/h.js
+++
@@ -1,9 +0,0 @@
-let e =document.getElementById("idSaludo");
-e.innerHTML;
-e.innerHTML= "adios hasta mañana ";
-e.innerHTML;
-
-
-let p = document.createElement("p");
-p.id="idPrueba"
-p.classList.add("importante");
