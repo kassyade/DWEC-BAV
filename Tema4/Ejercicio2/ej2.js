a82ef48a9f2b17a316bf9c0c59dce6e7ba485c43
kassyade <benjamin.aviles.std@gmail.com>
2024-10-29 12:13:01 +0100

    x

--- Tema4/Ejercicio2/ej2.js
+++ Tema4/Ejercicio2/ej2.js
@@ -2,4 +2,21 @@ function crearElemento(tipo,contenido,padre){
 let hijo = document.createElement(tipo)
 hijo.innerHTML =contenido
 padre.appendChild(hijo)
+return hijo; 
 }
+
+function insertarLi(){
+    let rand = Math.floor(Math.random() * 101)//crear un numero random de 0  a 100
+    console.log(rand)
+    let li = document.createElement("li")
+    li.innerHTML =`Numero nuevo :${rand} ` //recuerda usar las comillas al lado de la p
+    document.body.appendChild(li)
+}
+
+let bton=crearElemento("button","Insertar elemento",document.body)
+bton.addEventListener( "click",insertarLi)
+
+
+
+
+
